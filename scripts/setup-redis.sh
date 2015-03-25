b94f80078b10980d986e8f9802d86dd96c48c369
ΚΣΣGΔΠ ΒRΘΨΠ <brown.keegan@gmail.com>
2015-03-25 10:26:54 -0500

    auto accept install (-y)

--- scripts/setup-redis.sh
+++ scripts/setup-redis.sh
@@ -1,5 +1,5 @@
-apt-get install build-essential
-apt-get install tcl8.5
+apt-get install build-essential -y
+apt-get install tcl8.5 -y
 wget http://download.redis.io/releases/redis-2.8.19.tar.gz
 tar xzf redis-2.8.19.tar.gz
 cd redis-2.8.19
