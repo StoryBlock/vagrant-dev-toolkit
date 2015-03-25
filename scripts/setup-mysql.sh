09a85b07a1181800c3ca8f16ffca21f04ecb02bb
ΚΣΣGΔΠ ΒRΘΨΠ <brown.keegan@gmail.com>
2015-03-25 10:25:40 -0500

    auto accept install (-y)

--- scripts/setup-mysql.sh
+++ scripts/setup-mysql.sh
@@ -1,3 +1,3 @@
 debconf-set-selections <<< 'mysql-server mysql-server/root_password root root'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again root root'
-apt-get install mysql-server
+apt-get install mysql-server -y
