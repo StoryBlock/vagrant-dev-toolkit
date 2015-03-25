ee2f447e504f1bbbc10bc6dd5f351d9e2dd11f98
ΚΣΣGΔΠ ΒRΘΨΠ <brown.keegan@gmail.com>
2015-03-25 10:25:58 -0500

    auto accept install (-y)

--- scripts/setup-node.sh
+++ scripts/setup-node.sh
@@ -1,4 +1,4 @@
-apt-get install gcc make build-essential
+apt-get install gcc make build-essential -y
 apt-get install -y curl
 apt-get install -y git
 curl -sL https://deb.nodesource.com/setup | sudo bash -
