bb4946865490def05d64ed843bb8ed489c073b9b
ΚΣΣGΔΠ ΒRΘΨΠ <brown.keegan@gmail.com>
2015-03-25 10:26:32 -0500

    auto accept install (-y)

--- scripts/setup-php.sh
+++ scripts/setup-php.sh
@@ -1,2 +1,2 @@
-apt-get install php5 libapache2-mod-php5 php5-mcrypt
-apt-get install php5-curl  php5-mysql
+apt-get install php5 libapache2-mod-php5 php5-mcrypt -y
+apt-get install php5-curl php5-mysql -y
