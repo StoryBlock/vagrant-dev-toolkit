5e83454d489f3a6c86e5849df99a53f62d9a4054
ΚΣΣGΔΠ ΒRΘΨΠ <brown.keegan@gmail.com>
2015-10-15 09:54:36 -0500

    Update setup-node.sh

--- scripts/setup-node.sh
+++ scripts/setup-node.sh
@@ -1,6 +1,12 @@
-apt-get install gcc make build-essential -y
-apt-get install -y curl
-apt-get install -y git
-curl -sL https://deb.nodesource.com/setup | sudo bash -
-apt-get install -y nodejs
+sudo apt-get install gcc make build-essential -y
+sudo apt-get install -y curl
+sudo apt-get install -y git
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+echo "source /home/vagrant/.nvm/nvm.sh" >> /home/vagrant/.profile
+source /home/vagrant/.profile
+nvm install v0.12
+nvm install v4
+nvm install stable
+nvm alias default stable
 
+n=$(which node);n=${n%/bin/node}; sudo chmod -R 755 $n/bin/*; sudo cp -r $n/{bin,lib,share} /usr/local
