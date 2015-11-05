18c4a72cb08aa05357f5951c6333b6b26a0acada
ΚΣΣGΔΠ ΒRΘΨΠ <brown.keegan@gmail.com>
2015-11-05 17:48:59 -0600

    Update setup-mysql.sh

--- scripts/setup-mysql.sh
+++ scripts/setup-mysql.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password root root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again root root'
 sudo apt-get install mysql-server -y
